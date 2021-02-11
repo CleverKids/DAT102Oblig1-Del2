842764f3ffc15c375d96f55b7cdb3e8cf858b7a2
594620 <>
2021-02-11 17:12:47 +0100

    Update KlientKoe.java

--- src/no/hvl/dat102/klient/KlientKoe.java
+++ src/no/hvl/dat102/klient/KlientKoe.java
@@ -3,6 +3,8 @@ package no.hvl.dat102.klient;
 import no.dat102.tabell.TabellKoe;
 import no.hvl.dat102.adt.KoeADT;
 import no.hvl.dat102.exception.EmptyCollectionException;
+import no.hvl.dat102.kjedet.KjedetKoe;
+import no.hvl.dat102.sirkulaer.SirkulaerKoe;
 
 public class KlientKoe {
 
@@ -10,7 +12,9 @@ public class KlientKoe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		KoeADT<Character> tegnKoe = new TabellKoe<>();
+		//KoeADT<Character> tegnKoe = new TabellKoe<>();
+		//KoeADT<Character> tegnKoe = new SirkulaerKoe<>();
+		KoeADT<Character> tegnKoe = new KjedetKoe<>();
 		//Bytte ut med KjedetKoe og med SirkulaerKoe
 		String streng = " Denne koen er en FIFO datastruktur.";
 		int lengde = streng.length();
